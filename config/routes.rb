5dfa795613bb7841d9d5623699890de8da4aff60
YaHack94 <yahack94@gmail.com>
2021-11-24 22:20:37 +0100

    Bug Fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root 'publications#index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'publications#index'
   resources :publications do
     resources :comments
   end
